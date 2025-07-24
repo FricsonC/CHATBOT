ab130f27c7731bf468d1e452a2172df0ed012379
FricsonC <carvajallandetaf@gmail.com>
2025-07-24 15:16:18 -0500

    Update dialogflowRoutes.js

--- src/routes/dialogflowRoutes.js
+++ src/routes/dialogflowRoutes.js
@@ -3,6 +3,6 @@ import { enviarMensaje } from '../controladores/dialogflowCtrl.js';
 
 const router = express.Router();
 
-router.post('/', enviarMensaje); // POST /api/dialogflow
+router.post('/chatbot', enviarMensaje); // POST /api/dialogflow
 
 export default router;
